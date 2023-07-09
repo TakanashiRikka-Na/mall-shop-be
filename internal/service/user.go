d540edf0f486bd17441fd393a6337ebc9f051e6d
TakanashiRikka-Na <2576496262@qq.com>
2023-07-09 11:56:49 +0800

    feat: new user service

---
+++ internal/service/user.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"context"
+	"mall-shop-be/internal/biz"
+
+	pb "mall-shop-be/api/helloworld/v1"
+)
+
+type UserService struct {
+	pb.UnimplementedUserServer
+	uc *biz.UserUseCase
+}
+
+func NewUserService(uc *biz.UserUseCase) *UserService {
+	return &UserService{uc: uc}
+}
+
+func (s *UserService) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterReply, error) {
+	return &pb.RegisterReply{}, nil
+}
+func (s *UserService) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginReply, error) {
+	return &pb.LoginReply{}, nil
+}
