b1086e87ca4880d151052b97f7ebe475457fda22
TakanashiRikka-Na <2576496262@qq.com>
2023-07-13 21:04:37 +0800

    feat: add cursor redis module

--- internal/data/redis.go
+++ internal/data/redis.go
@@ -3,6 +3,7 @@ package data
 import (
 	"encoding/json"
 	"github.com/go-redis/redis"
+	"log"
 	"time"
 )
 
@@ -28,6 +29,56 @@ func ReadCache[T interface{}](key string, rdb *redis.Client, data *T) error {
 	return nil
 }
 
-func DeleteCache(key string, rdb *redis.Client) error {
-	return rdb.Del(key).Err()
+func DeleteCache(rdb *redis.Client, keys ...string) error {
+	for _, key := range keys {
+		if err := rdb.Del(key).Err(); err != nil {
+			//如果错误原因是空值就 continue
+			if err.Error() == "redis: nil" {
+				continue
+			}
+			log.Println(err)
+			return err
+		}
+	}
+	return nil
+}
+
+// 游标分页
+
+func StoreCursor(rdb *redis.Client, cursor string, id int) error {
+	err := rdb.Set("cursor", cursor, 0).Err()
+	if err != nil {
+		return err
+	}
+	err = rdb.Set(cursor, id, 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func StorePageResult[T interface{}](rdb *redis.Client, pages []T, key string, pageId int) error {
+	var z []redis.Z
+	for _, page := range pages {
+		z = append(z, redis.Z{Score: float64(pageId), Member: page})
+	}
+	return rdb.ZAdd(key, z...).Err()
+
+}
+
+func GetPagesResult[T interface{}](rdb *redis.Client, key string) ([]T, error) {
+	results, err := rdb.ZRange(key, 0, -1).Result()
+	if err != nil {
+		return nil, err
+	}
+	var ts []T
+	for _, result := range results {
+		var t T
+		err = json.Unmarshal([]byte(result), &t)
+		if err != nil {
+			return nil, err
+		}
+		ts = append(ts, t)
+	}
+	return ts, nil
 }
